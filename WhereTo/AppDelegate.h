bbe3dde3a0fa74960e41185a6584dd53dce3ea19
Nick Perkins <nickperkins@protonmail.com>
2016-04-27 09:24:03 -0400

    Initial Commit

---
+++ WhereTo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereTo
+//
+//  Created by Nick Perkins on 4/27/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhereTo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhereTo
+//
+//  Created by Nick Perkins on 4/27/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
